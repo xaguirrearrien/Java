a1dd4312d0cd0eb454d49d3c6b69d6fa461d486d
Xabi <xaguirrearrien@gmail.com>
2021-05-28 18:17:20 +0200

    calculadora simple

--- Basico/src/basicos/calculadora.java
+++ Basico/src/basicos/calculadora.java
@@ -7,16 +7,18 @@ public class calculadora {
 		
 		int n1 = 0;
 		int n2 = 0;
-		int resultado = 0;
 		int opcion = 0;
+		int resultado = 0;
 		
-		Scanner reader = new Scanner(System.in);
+		Scanner lector;
+		
+		lector = new Scanner(System.in);
 		
 		System.out.println("Introduce el primer número:");                     
-        n1 = reader.nextInt();
+        n1 = lector.nextInt();
         
         System.out.println("Introduce el segundo numero:");
-        n2 = reader.nextInt();
+        n2 = lector.nextInt();
         
         System.out.println("Selecciona una de las siguientes opciones:"
         		+ "\n 1.Sumar"
@@ -25,21 +27,25 @@ public class calculadora {
         		+ "\n 4.Dividir"
         		+ "\n 5.Salir");
         
-        opcion = reader.nextInt();
+        opcion = lector.nextInt();
         
         switch(opcion){
         	
         	case 1:
-        		System.out.println("Sumar");
+        		resultado = sumar(n1, n2);
+        		System.out.println("El resultado de la suma es: "+resultado);
         		break;
         	case 2:
-        		System.out.println("Restar");
+        		resultado = restar(n1, n2);
+        		System.out.println("El resultado de la resta es: "+resultado);
         		break;
         	case 3:
-        		System.out.println("Multiplicar");
+        		resultado = multiplicar(n1, n2);
+        		System.out.println("El resultado de la multiplicacion es: "+resultado);
         		break;
         	case 4:
-        		System.out.println("Dividir");
+        		resultado = dividir(n1, n2);
+        		System.out.println("El resultado de la division es: "+resultado);
         		break;
         	case 5:
         		System.out.println("Adios, hasta la proxima...");
@@ -48,6 +54,30 @@ public class calculadora {
         }
 		
 
+	}
+	
+	public static int sumar(int n1, int n2) {
+		
+		return n1+n2;
+		
+	}
+	
+	public static int restar(int n1, int n2) {
+		
+		return n1-n2;
+		
+	}
+
+	public static int multiplicar(int n1, int n2) {
+	
+		return n1*n2;
+	
+	}
+
+	public static int dividir(int n1, int n2) {
+	
+		return n1/n2;
+	
 	}
 
 }
